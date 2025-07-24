af6a3bcd007a6668c1a2782d4c1ff05bf0ff8a23
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-07-24 12:44:12 +0000

    change path for routes

--- src/components/UI/appRouter.jsx
+++ src/components/UI/appRouter.jsx
@@ -13,16 +13,16 @@ import Setting from "../../pages/Setting/Setting";
 const AppRouter = () =>{
     return(
         <Routes>
-          <Route exact path="/bankdash/" element={<Dashboard></Dashboard>}></Route>
-          <Route exact path="/bankdash/dashboard" element={<Dashboard></Dashboard>}></Route>
-          <Route exact path="/bankdash/transactions" element={<Transactions></Transactions>}></Route>
-          <Route exact path="/bankdash/accounts" element={<Accounts></Accounts>}></Route>
-          <Route exact path="/bankdash/investments" element={<Investments></Investments>}></Route>
-          <Route exact path="/bankdash/credit-cards" element={<CreditCards></CreditCards>}></Route>
-          <Route exact path="/bankdash/loans" element={<Loans></Loans>}></Route>
-          <Route exact path="/bankdash/services" element={<Services></Services>}></Route>
-          <Route exact path="/bankdash/my-privileges" element={<Privileges></Privileges>}></Route>
-          <Route exact path="/bankdash/setting" element={<Setting></Setting>}></Route>
+          <Route exact path="/bank-dash/" element={<Dashboard></Dashboard>}></Route>
+          <Route exact path="/bank-dash/dashboard" element={<Dashboard></Dashboard>}></Route>
+          <Route exact path="/bank-dash/transactions" element={<Transactions></Transactions>}></Route>
+          <Route exact path="/bank-dash/accounts" element={<Accounts></Accounts>}></Route>
+          <Route exact path="/bank-dash/investments" element={<Investments></Investments>}></Route>
+          <Route exact path="/bank-dash/credit-cards" element={<CreditCards></CreditCards>}></Route>
+          <Route exact path="/bank-dash/loans" element={<Loans></Loans>}></Route>
+          <Route exact path="/bank-dash/services" element={<Services></Services>}></Route>
+          <Route exact path="/bank-dash/my-privileges" element={<Privileges></Privileges>}></Route>
+          <Route exact path="/bank-dash/setting" element={<Setting></Setting>}></Route>
         </Routes>
     )
 }
