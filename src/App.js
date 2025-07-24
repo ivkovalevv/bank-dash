04dc030e05fa51319797a79a18598efb6aef27b6
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-07-24 12:26:09 +0000

    удалены лишние импорты

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/UI/Navbar/Navbar';
 import AppRouter from './components/UI/appRouter';
-import Header from './components/UI/Header/Header';
-import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
