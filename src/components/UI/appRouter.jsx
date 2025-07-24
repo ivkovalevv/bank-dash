c5bc20745cd6aef9ddf775fc98be769895075dee
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-07-24 12:57:55 +0000

    fix routes in navbar

--- src/components/UI/appRouter.jsx
+++ src/components/UI/appRouter.jsx
@@ -13,16 +13,16 @@ import Setting from "../../pages/Setting/Setting";
 const AppRouter = () =>{
     return(
         <Routes>
-          <Route exact path="/" element={<Dashboard></Dashboard>}></Route>
-          <Route exact path="/dashboard" element={<Dashboard></Dashboard>}></Route>
-          <Route exact path="/transactions" element={<Transactions></Transactions>}></Route>
-          <Route exact path="/accounts" element={<Accounts></Accounts>}></Route>
-          <Route exact path="/investments" element={<Investments></Investments>}></Route>
-          <Route exact path="/credit-cards" element={<CreditCards></CreditCards>}></Route>
-          <Route exact path="/loans" element={<Loans></Loans>}></Route>
-          <Route exact path="/services" element={<Services></Services>}></Route>
-          <Route exact path="/my-privileges" element={<Privileges></Privileges>}></Route>
-          <Route exact path="/setting" element={<Setting></Setting>}></Route>
+          <Route path="/" element={<Dashboard></Dashboard>}></Route>
+          <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+          <Route path="/transactions" element={<Transactions></Transactions>}></Route>
+          <Route path="/accounts" element={<Accounts></Accounts>}></Route>
+          <Route path="/investments" element={<Investments></Investments>}></Route>
+          <Route path="/credit-cards" element={<CreditCards></CreditCards>}></Route>
+          <Route path="/loans" element={<Loans></Loans>}></Route>
+          <Route path="/services" element={<Services></Services>}></Route>
+          <Route path="/my-privileges" element={<Privileges></Privileges>}></Route>
+          <Route path="/setting" element={<Setting></Setting>}></Route>
         </Routes>
     )
 }
